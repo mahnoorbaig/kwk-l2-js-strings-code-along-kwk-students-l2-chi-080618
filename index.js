277c16167e0248a9dc24b1d91efd5349404d6d29
mahnoorbaig <mahnoorbaig.contact@gmail.com>
2018-08-08 16:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const welcomeMessage = 'Welcome to Klossybook, ' + currentUser + '!';
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ' + currentUser[0];
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
 
